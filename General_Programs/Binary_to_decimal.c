640effeb0609fd4ef71dda8d5da5eb27c3d74955
manurag20 <33825737+manurag20@users.noreply.github.com>
2026-02-26 17:16:36 +0530

    Implement binary to decimal conversion function
    Added a function to convert binary strings to decimal integers.

--- General_Programs/Binary_to_decimal.c
+++ General_Programs/Binary_to_decimal.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
 
+int Dnum(char *s){
+    int len  = strlen(s);
+    int Dnumb=0;
+    for (int i=0; i<len;i++){
+        printf("\nthe character is: %c and i is: %d",s[len-i-1],i);
+        Dnumb = Dnumb +((s[len-i-1] - '0')*pow(2,i));
+        printf("Dnum is: %d", Dnumb);
+    }
+    return Dnumb;
+}
+int main() {
+    char *num = "11011";
+    printf("\nThe number is : %d",Dnum(num));
+    return 0;
+}
